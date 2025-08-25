7b86a689e84d4648693bed38a704e8359ed21439
黃紋吸蜜 <weijen.wang@tpisoftware.com>
2025-08-25 19:19:38 +0800

    feat: add an empty tableview to the main page

--- O_N_HW/ViewController.swift
+++ O_N_HW/ViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mainDataTableView: UITableView!
+    
     private let mockSocket = WebSocketMock()
     
     private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainDataTableView.delegate = self
+        mainDataTableView.dataSource = self
         
         bindViewModel()
         
@@ -39,3 +44,14 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+}

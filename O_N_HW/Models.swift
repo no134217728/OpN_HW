3311ca8ea46572fa0736f5f617012de31d542ea9
黃紋吸蜜 <weijen.wang@tpisoftware.com>
2025-08-25 16:38:52 +0800

    feat: add mock basic

---
+++ O_N_HW/Models.swift
@@ -0,0 +1,21 @@
+//
+//  Models.swift
+//  O_N_HW
+//
+//  Created by 黃紋吸蜜 on 2025/8/25.
+//
+
+import Foundation
+
+struct Match: Decodable {
+    let matchID: Int
+    let teamA: String
+    let teamB: String
+    let startTime: String
+}
+
+struct Odds: Decodable {
+    let matchID: Int
+    let teamAOdds: Decimal
+    let teamBOdds: Decimal
+}
---
+++ O_N_HW/WebSocketMock.swift
@@ -0,0 +1,38 @@
+//
+//  WebSocketMock.swift
+//  O_N_HW
+//
+//  Created by 黃紋吸蜜 on 2025/8/25.
+//
+
+import UIKit
+import Combine
+
+class WebSocketMock {
+    var resultPublisher: AnyPublisher<Odds, Never> { result.eraseToAnyPublisher() }
+    
+    private let result = PassthroughSubject<Odds, Never>()
+    
+    func socketPush() {
+        let number = Int.random(in: 1...10)
+        
+        for i in 0...number {
+            let odds = generateRandomOdds()
+            result.send(odds)
+        }
+    }
+    
+    func generateRandomOdds() -> Odds {
+        let matchID = Int.random(in: 1001...1120)
+        var oddsA = Decimal(Double.random(in: 1...3))
+        var oddsB = Decimal(Double.random(in: 1...3))
+        var roundedA = Decimal()
+        var roundedB = Decimal()
+        NSDecimalRound(&roundedA, &oddsA, 2, .plain)
+        NSDecimalRound(&roundedB, &oddsB, 2, .plain)
+        
+        return Odds(matchID: matchID,
+                    teamAOdds: roundedA,
+                    teamBOdds: roundedB)
+    }
+}
